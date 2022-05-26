14fe720b3f3aede9488526fc403de403a4a168f4
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2022-05-26 11:02:42 -0300

    to ajeitando a lojinha

--- Aula 12/Lojinha/src/App.java
+++ Aula 12/Lojinha/src/App.java
@@ -3,8 +3,12 @@ public class App {
         Produto cornDog = new Comida(12.5,"CornDog",5,"Um cachorro quente meio errado",EnumCategoriaComida.COREANA,EnumAlergicos.GLUTEN,EnumPimenta.SUAVE);
         Produto acaiMoleza = new Bebida(10.5,"Acai do moleza" , 1, "Real Fonte de energia", EnumCategoriaBebida.ENERGETICO, EnumTemperatura.FRIO);
 
-        System.out.println("Preços Regulares: ");
-        System.out.println(cornDog.getNome()+  "\t R$" + cornDog.getPreco());
-        System.out.println(acaiMoleza.getNome()+"\t R$"+acaiMoleza.getPreco());
+        System.out.println("Preços Com Desconto: ");
+        System.out.println(cornDog.getNome()+  "\t R$" + precoComDesconto(cornDog));
+        System.out.println(acaiMoleza.getNome()+"\t R$"+precoComDesconto(acaiMoleza));
+    }
+
+    public static String precoComDesconto(IGerarDesconto produto){
+        return "R$"+produto.gerarDesconto();
     }
 }
