2301e7d943933530055b1d7619bbb7119add24b2
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2022-08-11 10:53:29 -0300

    Carrinho em PY

--- Aula15/main.py
+++ Aula15/main.py
@@ -10,13 +10,26 @@ valor=350.0,
 nome='Stray R$ ',
 descricao=' gato fodendo geral =)')
 
+item3 = Item(
+valor=350.0,
+nome='Stray R$ ',
+descricao=' gato fodendo geral =)')
+
+# carrinho = Carrinho()
+
+# print(f'Tamanho: {carrinho.get_quantidade_itens()}')
+# print(f'Valor: {carrinho.get_valor_total()}')
+
+# carrinho.adicionar(item1)
+# carrinho.adicionar(item2)
+
+# print(f'Tamanho: {carrinho.get_quantidade_itens()}')
+# print(f'Valor: {carrinho.get_valor_total()}')
+
 carrinho = Carrinho()
 
-print(f'Tamanho: {carrinho.get_quantidade_itens()}')
-print(f'Valor: {carrinho.get_valor_total()}')
+print(item1 == item2)
+print(item1 == item1)
+print(item2 == item3)
 
-carrinho.adicionar(item1)
-carrinho.adicionar(item2)
-
-print(f'Tamanho: {carrinho.get_quantidade_itens()}')
-print(f'Valor: {carrinho.get_valor_total()}')
+print(item2 == carrinho)
