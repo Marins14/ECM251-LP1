29dee5b770b9093f9278cfecb170ab51646abb26
Marins14 <matheus.bernardello@live.com>
2022-08-17 10:53:12 -0300

    Aula_16

--- Aula 16/main.py
+++ Aula 16/main.py
@@ -3,14 +3,14 @@ from modelos.plantas import Arvore, Alface
 
 def run_system():
     doc1 = Document()
-    doc2 = Email(authors=['Matheus Marins Bernardello'])
-    doc3 = Book(authors =["Jonathan","Michelle","Gabriel"])
-    print(doc2.get_authors())
+    doc2 = Email(to='20.00286-6@maua.br',authors=['Matheus Marins Bernardello'])
+    doc3 = Book(title= "Galerinha", authors =["Jonathan","Michelle","Gabriel"])
+    print(doc2)
     print(doc3)
     
     
 if __name__ == "__main__":
-    # planta1 = Arvore('Carvalho')
+    # planta1 = Arvore('Caralho')
     # planta2 = Alface(nome='Hamburguer de siri do futuro')
 
     # print(planta1.ola())
