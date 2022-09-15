fcedcdf0d0e6e98f635e94c3e86ca921a42b2e0f
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2022-09-15 10:45:17 -0300

    Lógica da inversão de imagem

--- Aula20-StreamLit/src/pages/nova_pag.py
+++ Aula20-StreamLit/src/pages/nova_pag.py
@@ -1,15 +1,27 @@
 import streamlit as st 
-
-st.title("HOME 🤗")
-st.write("Estou começando a fazer a minha lojinha, fé que sai 😎")
-st.markdown(" ## Os novos jogos estão sempre em exposição!")
+if "baitado" not in st.session_state:
+    st.session_state["baitado"] = False
 
 def baitado():
-    st.image(image = "assets/confia.jpg", caption = "VOCE FOI BAITADO! 🤣")
-st.button(
-    label= "CLICA AEW MEU JOVI 😎",
-    help="CONFIA🤑",
-    on_click = baitado
-)
+    st.session_state["baitado"] = not st.session_state["baitado"]
+
+main, trolei = st.tabs(["HOME","AH NEGAO"])
+with main:
+    st.title("HOME 🤗")
+    st.write("Estou começando a fazer a minha lojinha, fé que sai 😎")
+    st.markdown(" ## Os novos jogos estão sempre em exposição!")
+    st.write("Loja em desenvolvimento...Aguarde!")
+
+with trolei:
+    st.button(
+        label= "CLICA AEW MEU JOVI 😎",
+        help="TEM CERTEZA DISSO ?🤪",
+        on_click = baitado
+    )
+    if st.session_state["baitado"]:
+        st.image(image = "assets/confia.jpg", caption = "VOCE FOI BAITADO! 🤣")
+
+
+
 
 st.sidebar.title("Pagamentos 🤑")
