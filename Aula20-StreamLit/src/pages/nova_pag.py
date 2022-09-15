a6ed3be62a038255afeff48031ffcb6e933766e8
MATHEUS MARINS BERNARDELLO <20.00286-6@maua.br>
2022-09-15 10:28:06 -0300

    Add algumas coisas na pag nova

--- Aula20-StreamLit/src/pages/nova_pag.py
+++ Aula20-StreamLit/src/pages/nova_pag.py
@@ -1,4 +1,15 @@
 import streamlit as st 
 
-st.title("Pagina nova")
-st.sidebar.title("Só um testezin")
+st.title("HOME 🤗")
+st.write("Estou começando a fazer a minha lojinha, fé que sai 😎")
+st.markdown(" ## Os novos jogos estão sempre em exposição!")
+
+def baitado():
+    st.image(image = "assets/confia.jpg", caption = "VOCE FOI BAITADO! 🤣")
+st.button(
+    label= "CLICA AEW MEU JOVI 😎",
+    help="CONFIA🤑",
+    on_click = baitado
+)
+
+st.sidebar.title("Pagamentos 🤑")
