d7488d0fbc74fa4633f34a1de2f507bc82356b1d
Marins14 <matheus.bernardello@live.com>
2022-09-26 14:36:58 -0300

    começo do projeto

---
+++ Atividade_T3/src/models/product.py
@@ -0,0 +1,11 @@
+from cgi import print_exception
+from unicodedata import name
+
+
+class Product():
+    def __init__(self,name,price,url) -> None:
+        self.name = name
+        self.price = price 
+        self.url = url
+    def __str__(self) -> str:
+        return f'Product(name):{self.name}, price:{self.price}, url:{self.url}'
