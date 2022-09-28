31b90b1ff188668be1ee850218cc1b2d27a5009b
Marins14 <matheus.bernardello@live.com>
2022-09-28 17:33:13 -0300

    exibir_Itens com problema

--- Atividade_T3/src/models/carrinho.py
+++ Atividade_T3/src/models/carrinho.py
@@ -8,18 +8,23 @@ class Carrinho():
         for item in self._itens:
             total += item.get_Valor()
         return total
+        
+    def exibir_Itens(self,item):
+        return self._itens[item]
+    
     def get_Quantidade_Itens(self):
         return len(self._itens)
+    
     def adicionar(self,item):
         self._itens.append(item)
+    
     def remover(self,item):
         if item in self._itens:
             self._itens.remove(item)
+    
     def get_Quantidade_Itens_Repetidos(self,objetivo):
         contador = 0
         for item in self._itens:
             if objetivo == item:
                 contador += 1
-    def exibir_Itens(self):
-        for item in self._itens:
-            return item
+        return contador
--- Atividade_T3/src/models/product.py
+++ Atividade_T3/src/models/product.py
@@ -3,9 +3,10 @@ from unicodedata import name
 
 
 class Product():
-    def __init__(self,name,descricao,price,url) -> None:
+    def __init__(self,name,descricao,keyword,price,url) -> None:
         self._name = name
         self._descricao = descricao
+        self._keyword = keyword
         self._price = price 
         self._url = url
     # Getters da classe
@@ -13,6 +14,8 @@ class Product():
         return self._name
     def get_Descricao(self):
         return self._descricao
+    def get_Keyword(self):
+        return self._keyword
     def get_Valor(self):
         return self._price
     def get_Imagem(self):
