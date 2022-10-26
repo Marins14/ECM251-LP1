1fbf2210236fbdee767526fec9f001b28f84fd90
Marins14 <matheus.bernardello@live.com>
2022-10-26 09:21:37 -0300

    Funcionalidades novas

--- Projeto-sqlite/tests/teste_controller_pedido.py
+++ Projeto-sqlite/tests/teste_controller_pedido.py
@@ -6,3 +6,14 @@ resultado = controller.pegar_pedido("001")
 print(resultado)
 for elemento in resultado:
     print(elemento)
+    
+item_pedido = resultado[0]
+item_pedido.quantidade = 10
+controller.atualizar_pedido(item_pedido)
+print("###########################################")
+
+resultado = controller.pegar_pedido("001")
+print(resultado)
+for elemento in resultado:
+    print(elemento)
+    
